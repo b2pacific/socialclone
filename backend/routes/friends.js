7497a17df8867cfcaa3805a48c2ade948d1ad332
b2pacific <b2pacific@gmail.com>
2020-09-22 13:59:10 +0530

    Create migration-config file

--- backend/routes/friends.js
+++ backend/routes/friends.js
@@ -4,9 +4,11 @@ const User = require("../models/users");
 const Friend = require("../models/friendship");
 const auth = require("../authenticate");
 
-router.get('/', auth, function(req, res, next) {
-    Friend.findOne({userid: req.user._id}).populate('friends').exec(function(error, friends) {
-        if(!friends)
+router.get('/', auth, function (req, res, next) {
+    Friend.findOne({
+        userid: req.user._id
+    }).populate('friends').exec(function (error, friends) {
+        if (!friends)
             res.json({
                 message: "No friends"
             });
@@ -17,35 +19,83 @@ router.get('/', auth, function(req, res, next) {
     })
 });
 
-router.post('/', auth, function(req, res, next) {
-    const friend=req.body.friend;
+router.post('/', auth, function (req, res, next) {
+    const friend = req.body.friend;
 
-    User.findOne({username: friend}, function(error, user) {
-            
-        if(!user)
+    User.findOne({
+        username: friend
+    }, function (error, user) {
+
+        if (!user)
             return res.err(error);
-    
-            Friend.findOne({userid: req.user._id}, function(error, friend) {
-            
-                if(!friend)
-                    return res.err(error);
-            
-                friends = friend.friends;
-                
-                friends.push(user._id);
-        
-                Friend.findOneAndUpdate({"_id": friend._id}, {"friends": friends}, {upsert: true}, function(err, friend) {
-                    if(err)
-                        return res.err(err);
-                    return res.json({
-                        message: "Successfull"
-                    });
-                })
-            })
 
-        
+        Friend.findOne({
+            userid: req.user._id
+        }, function (error, friend) {
+
+            if (!friend)
+                return res.err(error);
+
+            friends = friend.friends;
+
+            friends.push(user._id);
+
+            Friend.findOneAndUpdate({
+                "_id": friend._id
+            }, {
+                "friends": friends
+            }, {
+                upsert: true
+            }, function (err, friend) {
+                if (err)
+                    return res.err(err);
+                return res.json({
+                    message: "Successfull"
+                });
+            })
+        })
     })
-    
+
+})
+
+
+router.post('/unfriend', auth, function (req, res, next) {
+    const friend = req.body.friend;
+
+    User.findOne({
+        username: friend
+    }, function (error, user) {
+
+        if (!user)
+            return res.err(error);
+
+        Friend.findOne({
+            userid: req.user._id
+        }, function (error, friend) {
+
+            if (!friend)
+                return res.err(error);
+
+            friends = friend.friends;
+
+            friends.filter(function(value, index, arr){ return value!=user._id;});
+
+            Friend.findOneAndUpdate({
+                "_id": friend._id
+            }, {
+                "friends": friends
+            }, {
+                upsert: true
+            }, function (err, friend) {
+                if (err)
+                    return res.err(err);
+                return res.json({
+                    message: "Successfull"
+                });
+            })
+        })
+    })
+
 })
 
 module.exports = router;
--- backend/routes/tweet.js
+++ backend/routes/tweet.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const User = require("../models/users");
 const Tweet = require("../models/tweets");
 const auth = require("../authenticate");
+const Friend = require("../models/friendship");
 
 router.get('/', auth, function(req, res, next) {
     Tweet.find({"author": req.user._id}, function(error, tweets) {
@@ -18,7 +19,25 @@ router.get('/', auth, function(req, res, next) {
 });
 
 router.get('/all', auth, function(req, res, next) {
-    friends = user.friends;
+    friend = req.user.friends;
+
+    Friend.findOne({_id: friend}, function(err, friend) {
+        friends=friend.friends;
+
+        Tweet.find({author: {$all: friends}}, function(err, tweets) {
+
+            if(!tweets)
+                return res.json({
+                    message: "No tweets"
+                })
+
+            return res.json({
+                tweets: tweets
+            });
+
+        })
+
+    })
 
 })
 
